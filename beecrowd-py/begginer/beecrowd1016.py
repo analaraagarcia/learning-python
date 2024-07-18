be4de5f6bd2958069235a002e030490de44656eb
Ana Lara Garcia <analaraagarcia@hotmail.com>
2024-07-18 09:32:26 -0300

    new beecrowd problems

---
+++ beecrowd-py/begginer/beecrowd1016.py
@@ -0,0 +1,5 @@
+dist = int(input())
+
+time = 2 * dist
+
+print(f'{time} minutos')
---
+++ beecrowd-py/begginer/beecrowd1017.py
@@ -0,0 +1,7 @@
+time = int(input())
+speed = int(input())
+
+dist = time * speed
+liters = dist / 12.0
+
+print(f'{liters:.3f}')
